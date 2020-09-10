e7a6964c7b47207b9026ca8129f635fa794f3aa3
karnakar <karnakar@ymail.com>
2020-09-10 00:39:34 -0400

    crud updated

---
+++ src/js/viewModels/dummydonotdelete.js
@@ -0,0 +1,20 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * dummydonotdelete module
+ */
+define(['ojs/ojcore', 'knockout'
+], function (oj, ko) {
+    /**
+     * The view model for the main content view template
+     */
+    function dummydonotdeleteContentViewModel() {
+        var self = this;
+    }
+    
+    return dummydonotdeleteContentViewModel;
+});
